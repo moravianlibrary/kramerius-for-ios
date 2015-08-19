44a3890308c325f8a5b2e4c9499f7f3dca150da8
Ondrej Vyhlidal <ondrej.vyhlidal@blackboard.com>
2015-08-19 14:57:37 +0200

    gitignore

--- MZK_iOS/Build/Intermediates/MZK_iOS.build/Debug-iphonesimulator/MZK_iOS.build/Objects-normal/x86_64/MZKCollectionsViewController.d
+++ MZK_iOS/Build/Intermediates/MZK_iOS.build/Debug-iphonesimulator/MZK_iOS.build/Objects-normal/x86_64/MZKCollectionsViewController.d
@@ -1,3 +1,4 @@
 dependencies: \
   /Users/OndrejVyhlidal/PersonalFolder/Projects/kramerius-for-ios/MZK_iOS/MZK_iOS/MZKCollectionsViewController.m \
-  /Users/OndrejVyhlidal/PersonalFolder/Projects/kramerius-for-ios/MZK_iOS/MZK_iOS/MZKCollectionsViewController.h
+  /Users/OndrejVyhlidal/PersonalFolder/Projects/kramerius-for-ios/MZK_iOS/MZK_iOS/MZKCollectionsViewController.h \
+  /Users/OndrejVyhlidal/PersonalFolder/Projects/kramerius-for-ios/MZK_iOS/MZK_iOS/MZKDatasource.h
