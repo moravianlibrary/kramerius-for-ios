1822bf122250063d867a0f63ceb1716e054166df
ovyhlidal <o.vyhlidal@gmail.com>
2016-01-04 08:26:14 +0100

    Settings Constants and UserDefaults

---
+++ MZK_iOS/MZK_iOS/MZKSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MZKSettingsViewController.h
+//  MZK_iOS
+//
+//  Created by OndrejVyhlidal on 21/12/15.
+//  Copyright © 2015 Ondrej Vyhlidal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MZKSettingsViewController : UIViewController
+
+@end
