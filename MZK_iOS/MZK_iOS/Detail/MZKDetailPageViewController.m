0c50d6dc5e1a630ec775d7bd5acdaa6707852d0f
ovyhlidal <o.vyhlidal@gmail.com>
2016-09-15 07:35:53 +0200

    no message

---
+++ MZK_iOS/MZK_iOS/Detail/MZKDetailPageViewController.m
@@ -0,0 +1,37 @@
+//
+//  MZKDetailPageViewController.m
+//  MZK_iOS
+//
+//  Created by OndrejVyhlidal on 07/09/16.
+//  Copyright © 2016 Ondrej Vyhlidal. All rights reserved.
+//
+
+#import "MZKDetailPageViewController.h"
+
+@interface MZKDetailPageViewController ()
+
+@end
+
+@implementation MZKDetailPageViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
